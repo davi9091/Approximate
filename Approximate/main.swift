d87b80db319c0d2fed36bb74c019a08eee1d0064
davi9091 <davi9091@gmail.com>
2018-10-31 06:04:47 +0700

    Added Lagrange Polynomial function and input of the start and the end of set of points.

--- Approximate/main.swift
+++ Approximate/main.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+func laGrangePolynomial(n: Int, x: Float, xi: Array <Float>, yi: Array <Float>) -> Float {
+    var ti: Float = 0;
+    var tj: Float = 1;
+
+    var i, j: Int;
+
+    for i in 0...n {
+        for j in 0...n {
+            if (i != j) {
+                tj = tj*((x - xi[j])/(xi[i] - xi[j]));
+
+            }
+        }
+
+        ti = ti + tj * yi[i];
+        tj = 1;
+
+    }
+
+    return ti;
+}
+
+var xStart, xEnd, x: Float;
+var n: Int;
+
+xStart = (readLine() as! NSString).floatValue;
+xEnd = (readLine() as! NSString).floatValue;
+x = (readLine() as! NSString).floatValue;
