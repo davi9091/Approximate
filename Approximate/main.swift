42ea0c6af6e38e5340e029ad63d3d2e1beac601a
davi9091 <davi9091@gmail.com>
2018-10-31 05:29:08 +0700

    Initial commit

---
+++ Approximate/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Approximate
+//
+//  Created by Arkady Rubtsov on 31/10/2018.
+//  Copyright © 2018 davi9091. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
